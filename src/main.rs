0ceba82af3d52db9d022f7eba400027cb2aea7fe
Ivan Paraskun <ig.paraskun@gmail.com>
2024-09-19 23:52:49 +0700

    upd: yates shuffler.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,24 @@
-fn main() {}
+use rand::{rngs::StdRng, RngCore, SeedableRng};
+
+fn shuffle<T: Clone>(v: &mut [T]) {
+    let mut rng = StdRng::from_entropy();
+
+    for i in 0..v.len() - 1 {
+        let rnd = rng.next_u32() as usize % (v.len() - i) + i;
+        (v[i], v[rnd]) = (v[rnd].clone(), v[i].clone());
+    }
+}
+
+fn main() {
+    let mut key: Vec<u8> = (0..33).collect();
+
+    shuffle(&mut key);
+
+    unsafe {
+        let _: Vec<u32> = key
+            .iter()
+            .map(|e| *e as u32 + 1072)
+            .inspect(|e| print!("{}", char::from_u32_unchecked(*e)))
+            .collect();
+    }
+}
